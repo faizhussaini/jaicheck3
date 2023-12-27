43d479a32fdd9d3199cca3f64b87a9cda64ae75b
Faiz_Hussaini <107918420+faizhussaini@users.noreply.github.com>
2023-12-28 01:02:17 +0530

    ui0990

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import Portfolio from "./components/Portfolio/Portfolio";
 import Testimonial from "./components/Testimonials/Testimonial";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
+import Gallery from './components/gallery/Portfolio'
 import { useContext } from "react";
 import { themeContext } from "./Context";
 function App() {
@@ -27,6 +28,7 @@ function App() {
       <Experience />
       <Works />
       <Portfolio />
+      <Gallery/>
       <Testimonial />
       <Contact />
       <Footer />
--- components/gallery/Portfolio.jsx
+++ components/gallery/Portfolio.jsx
@@ -9,14 +9,20 @@ import MusicApp from "../../img/musicapp.png";
 import { themeContext } from "../../Context";
 import img5 from '../../img/5.png';
 import img7 from '../../img/7.png';
+import g1 from '../../img/g1.jpg';
+import g2 from '../../img/g2.jpg';
+import g3 from '../../img/g3.jpg';
+import g4 from '../../img/g4.jpg';
+import g5 from '../../img/g5.jpg';
+import g7 from '../../img/g7.jpg';
 const Portfolio = () => {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
   return (
     <div className="portfolio" id="portfolio">
       {/* heading */}
-      <span style={{color: darkMode?'white': ''}}>Let Us Serve Uh</span>
-      <span>With Best We've Got</span>
+      <span style={{color: darkMode?'white': ''}}>Lets Share Some</span>
+      <span>Moments With Uh All!!</span>
 
       {/* slider */}
       <Swiper
@@ -26,22 +32,22 @@ const Portfolio = () => {
         className="portfolio-slider"
       >
         <SwiperSlide>
-          <img src={Sidebar} alt="" />
+          <img src={g1} alt="" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={Ecommerce} alt="" />
+          <img src={g2} alt="" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={MusicApp} alt="" />
+          <img src={g3} alt="" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={HOC} alt="" />
+          <img src={g4} alt="" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={img5} alt="" />
+          <img src={g5} alt="" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={img7} alt="" />
+          <img src={g7} alt="" />
         </SwiperSlide>
       </Swiper>
     </div>
